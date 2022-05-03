eb99713b2c2d4c5bdb04f7bf1503d0a9d5c86d2e
dclark3774 <55795713+dclark3774@users.noreply.github.com>
2022-05-03 16:32:27 -0600

    Create helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# import dependencies
+import logging
+from logging.handlers import RotatingFileHandler
+
+# Initialize log file
+logging.basicConfig(handlers=[RotatingFileHandler('/opt/plcnext/logs/hello.log', maxBytes=1000000, backupCount=1)],level=logging.INFO, format="[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s", datefmt='%Y-%m-%dT%H:%M:%S')
+
+# Main program execution
+if __name__ == '__main__':
+    # Infinite Loop
+    while 1:
+        # Logging a hello message to show the script is running
+        logging.info("Hello from init.d!")
+        # 10 second delay between logging events
+        time.sleep(10)
